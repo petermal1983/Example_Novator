ce6f7e923158c4c0d25a86fb8958f0eb61be671d
petermal1983 <peter1983@mail.ru>
2021-06-03 11:37:33 +0500

    Добавьте файлы проекта.

---
+++ Example_Novator/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Example_Novator
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DataGridTextColumn col1 = new DataGridTextColumn();
+        DataGridTextColumn col2 = new DataGridTextColumn();
+        DataGridTextColumn col3 = new DataGridTextColumn();
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            col1.Binding = new Binding("Second_Name");
+            col2.Binding = new Binding("First_Name");
+            col3.Binding = new Binding("Patr_Name");
+
+            EmpGrid.Columns.Add(col1);
+            EmpGrid.Columns.Add(col2);
+            EmpGrid.Columns.Add(col3);
+
+            col1.Header = "Фамилия";
+            col2.Header = "Имя";
+            col3.Header = "Отчество";
+
+            col1.Width = 200;
+            col2.Width = 200;
+            col3.Width = 200;
+
+            col2.CanUserSort = false;
+            col3.CanUserSort = false;
+
+            LoadData();
+        }
+
+        public void LoadData()
+        {
+
+            List<Employee> empList = new List<Employee>();
+            using (NOVATOREntities db = new NOVATOREntities())
+            {    
+                var Emp = db.Employees;
+                foreach (Employee i in Emp)
+                    EmpGrid.Items.Add(i);
+                db.SaveChanges();
+            }
+        }
+
+
+        private void onClickDel(object sender, RoutedEventArgs e)
+        { 
+            try
+            {
+                Employee emp = (Employee)EmpGrid.SelectedItem;  
+                using (NOVATOREntities db = new NOVATOREntities())
+                {
+                    var Em = db.Employees;
+                    Em.Attach(emp);
+                    Em.Remove(emp);
+                    db.SaveChanges();
+                }
+                MessageBox.Show("Сотрудник удален", "Удаление сотрудника");
+                EmpGrid.Items.Clear();
+                LoadData();
+            }
+            catch
+            {
+                MessageBox.Show("Выделите сотрудника, которого хотите удалить", "Удаление сотрудника");
+            }
+
+
+        }
+
+        private void onClickCreate(object sender, RoutedEventArgs e)
+        {
+            SecondWindow sc = new SecondWindow(this);
+            sc.Show();
+        }
+    }
+}
---
+++ Example_Novator/SecondWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Example_Novator
+{
+    /// <summary>
+    /// Логика взаимодействия для SecondWindow.xaml
+    /// </summary>
+    public partial class SecondWindow : Window
+    {
+        MainWindow window;
+        public SecondWindow(MainWindow window)
+        {
+            this.window = window;
+            InitializeComponent();
+        }
+        private void onClickSave(object sender, RoutedEventArgs e)
+        {
+            
+            Employee emp = new Employee();
+            emp.First_Name = this.Name.Text;
+            emp.Second_Name = this.Second_Name.Text;
+            emp.Patr_Name = this.Patr_Name.Text;
+            try
+            {
+                using (NOVATOREntities db = new NOVATOREntities())
+                {
+                    var Em = db.Employees;
+                    int max = Em.Max(Employee => Employee.ID_Employee);
+                    emp.ID_Employee = max + 1;
+                    Em.Add(emp);
+                    db.SaveChanges();
+                }
+                MessageBox.Show("Сотрудник добавлен", "Добавление сотрудника");
+                window.EmpGrid.Items.Clear();
+                window.LoadData();
+            }
+
+            catch
+            {
+
+            }
+            this.Close();
+
+        }
+    }
+}
